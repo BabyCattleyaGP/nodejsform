47b45380a9449f7ca00389b6fce232fe2d98bf04
BabyCattleyaGP <baby16001@mail.unpad.ac.id>
2019-09-27 20:13:28 +0700

    add form

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const path = require('path');
 const express = require('express');
 //use hbs view engine
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
 const app = express();
  
 //set dynamic views file
 app.set('views',path.join(__dirname,'views'));
 //set view engine
 app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //set public folder as static folder for static file
 app.use(express.static('public'));
 
@@ -17,17 +21,25 @@ app.use(express.static('public'));
 app.get('/',(req, res) => {
   //render file index.hbs
   res.render('index', {
-      name : "Baby Cattleya"
+      name : "Baby Cattleya",
+      npm : "140810160048"
   });
 });
- 
-//route untuk halaman home dengan parameter name
-app.get('/:name',(req, res) => {
-    //render file index.hbs
-    res.render('index',{
-      name : req.params.name
-    });
+
+//route untuk manampilkan form
+app.get('/post',(req, res) => {
+    //render file form.hbs
+    res.render('form');
   });
+   
+//route untuk submit form
+app.post('/post',(req, res) => {
+//render file form.hbs
+res.render('index',{
+    //ambil value dari textname
+    name : req.body.textname
+});
+});
  
 app.listen(8000, () => {
   console.log('Server is running at port 8000');
