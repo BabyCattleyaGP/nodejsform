5d3e86af6e452a9b00a3ad6485a9abe77c054dc0
BabyCattleyaGP <baby16001@mail.unpad.ac.id>
2019-09-27 23:11:00 +0700

    finished

--- app.js
+++ app.js
@@ -1,48 +1,92 @@
-//use path module
+const http = require("http");
+//module filestream
+const fs = require("fs");
+//module path
 const path = require('path');
-//use express module
+//module express js
 const express = require('express');
-//use hbs view engine
+//module hbs untuk view
 const hbs = require('hbs');
+//module body parser
 const bodyParser = require('body-parser');
+//module mutler
+const multer = require("multer");
+
+
 const app = express();
- 
-//set dynamic views file
+const httpServer = http.createServer(app);
+
+const PORT = process.env.PORT || 8000;
+
+httpServer.listen(8000, () => {
+  console.log(`Server berjalan di ${PORT}`);
+});
+
+//folder views menyimpan file view (html)
 app.set('views',path.join(__dirname,'views'));
 //set view engine
 app.set('view engine', 'hbs');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//set public folder as static folder for static file
+//set folder public sebagai folder yang berisi static file (css js)
 app.use(express.static('public'));
 
-//route untuk halaman home
 app.get('/',(req, res) => {
-  //render file index.hbs
-  res.render('index', {
-      name : "Baby Cattleya",
-      npm : "140810160048"
+    //render file (home)
+    res.render('input');
   });
+
+app.get("/image.png", (req, res) => {
+    res.sendFile(path.join(__dirname, "./uploads/image.png"));
 });
 
-//route untuk manampilkan form
-app.get('/post',(req, res) => {
-    //render file form.hbs
-    res.render('form');
-  });
-   
-//route untuk submit form
-app.post('/post',(req, res) => {
-//render file form.hbs
-res.render('index',{
-    //ambil value dari textname
-    name : req.body.name,
-    npm : req.body.npm,
-    email : req.body.email
+
+//error handling
+const handleError = (err, res) => {
+  res
+    .status(500)
+    .contentType("text/plain")
+    .end("Error! Coba Lagi");
+};
+
+const upload = multer({
+  dest: "/public"
 });
-});
- 
-app.listen(8000, () => {
-  console.log('Server is running at port 8000');
-});
+
+
+app.post(
+  "/upload",
+  upload.single("file" /*nama atribut di form*/),
+  (req, res) => {
+    const tempPath = req.file.path;
+    const targetPath = path.join(__dirname, "./uploads/image.png"); //simpan gambar di sana
+
+    if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+      fs.rename(tempPath, targetPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(200)
+          .render('tampil',{
+            //ambil value dari textname
+            name : req.body.name,
+            npm : req.body.npm,
+            email : req.body.email
+        });
+
+        app.get("/image.png", (req, res) => {
+            res.sendFile(path.join(__dirname, "./uploads/image.png"));  //tampil gambar di view
+          });
+
+      });
+    } else {
+      fs.unlink(tempPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(403)
+          .contentType("text/plain")
+          .end("Hanya file .png!");
+      });
+    }
+  }
+);
